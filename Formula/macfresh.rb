f50e57f673478d15b9f3f2fed5f0a09d8760e943
Arin Agrawal <79896874+arinagrawal05@users.noreply.github.com>
2025-12-12 01:44:11 +0530

    Update macfresh.rb

--- Formula/macfresh.rb
+++ Formula/macfresh.rb
@@ -1,9 +1,9 @@
 class Macfresh < Formula
   desc "Ultra-safe macOS cleanup tool that frees space without risking user data"
   homepage "https://github.com/arinagrawal05/macfresh"
-  url "https://github.com/arinagrawal05/macfresh/releases/download/v1.0.0/macfresh.sh"
-  version "1.0.0"
-  sha256 "69dba24ee440a93d6d9acde46a53e34a0b71b1633f9a960a909e5ac822876eb9"
+  url "https://github.com/arinagrawal05/macfresh/releases/download/v1.0.1/macfresh.sh"
+  version "1.0.1"
+  sha256 "4748cd8abdc89147b617bdc63dc3f9743fb3adec17fe80818eca99848ffc2a66"
 
   depends_on :macos
 
