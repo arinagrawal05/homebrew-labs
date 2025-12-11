520ab82171fd1185505467c01cd53ccce5f24cc2
Arin Agrawal <79896874+arinagrawal05@users.noreply.github.com>
2025-12-12 00:49:40 +0530

    Update SHA256 checksum for macfresh

--- Formula/macfresh.rb
+++ Formula/macfresh.rb
@@ -3,7 +3,7 @@ class Macfresh < Formula
   homepage "https://github.com/arinagrawal05/macfresh"
   url "https://github.com/arinagrawal05/macfresh/releases/download/v1.0.0/macfresh.sh"
   version "1.0.0"
-  sha256 "REPLACE_WITH_YOUR_SHA256"
+  sha256 "69dba24ee440a93d6d9acde46a53e34a0b71b1633f9a960a909e5ac822876eb9"
 
   depends_on :macos
 
