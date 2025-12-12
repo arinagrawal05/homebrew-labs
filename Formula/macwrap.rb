78e3e68b402724c28651b0c9e01b892a94a1590c
Arin Agrawal <79896874+arinagrawal05@users.noreply.github.com>
2025-12-13 01:57:19 +0530

    Update macwrap.rb

--- Formula/macwrap.rb
+++ Formula/macwrap.rb
@@ -4,23 +4,21 @@ class Macwrap < Formula
   url "https://github.com/arinagrawal05/macwrap/releases/download/v1.0.1/macwrap-1.0.1.tar.gz"
   sha256 "3c76f4b635a31b3677374eb7b79b4669f800cef463d05a94b6b3f766752534b0"
   license "MIT"
-
+  
   depends_on "python@3.12"
-
+  
   def install
-    # Explicitly install contents of macwrap/ directory
-    (libexec/"macwrap").install Dir["macwrap/*"]
-
-    # Install launcher script
-    bin.install libexec/"macwrap/bin/macwrap"
-
-    # Env setup
-    bin.env_script_all_files(libexec/"macwrap/bin",
-      :PYTHONPATH => libexec/"macwrap",
-      :PYTHONHOME => libexec/"macwrap"
+    # Install the contents of the macwrap directory directly to libexec
+    libexec.install Dir["macwrap/*"]
+    
+    # Create a wrapper script in bin that calls the actual binary
+    (bin/"macwrap").write_env_script(
+      libexec/"bin/macwrap",
+      PYTHONPATH: libexec,
+      PYTHONHOME: Formula["python@3.12"].opt_prefix
     )
   end
-
+  
   test do
     system "#{bin}/macwrap", "--version"
   end
