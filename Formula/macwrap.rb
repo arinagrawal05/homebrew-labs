d4df2b1b3c5f089c38e970fc3e09a8f68d06cacd
Arin Agrawal <79896874+arinagrawal05@users.noreply.github.com>
2025-12-13 01:30:28 +0530

    Update macwrap.rb

--- Formula/macwrap.rb
+++ Formula/macwrap.rb
@@ -2,7 +2,7 @@ class Macwrap < Formula
   desc "A Spotify Wrapped-style yearly analytics tool for your Mac"
   homepage "https://github.com/arinagrawal05/macwrap"
   url "https://github.com/arinagrawal05/macwrap/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "318482df71efd8339b1fe301102ca44ebd6b785dd7f127baf43941567da24b00"
+  sha256 "ad9dfef85e544d8674fab43f8545aa1922c7aa4a4772498a921a19acf19824a8"
   license "MIT"
 
   depends_on "python@3.12"
